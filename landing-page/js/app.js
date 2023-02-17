e1eaa741eff249c4e3868952154578fe393ac45e
abanoubsamir7 <121108572+abanoubsamir7@users.noreply.github.com>
2023-02-17 21:02:46 +0200

    Add files via upload

---
+++ landing-page/js/app.js
@@ -0,0 +1,100 @@
+
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+
+
+/**
+ * Define Global Variables
+ *
+*/
+
+// Build menu
+const sections = document.querySelectorAll('section')
+const navi = document.getElementById('navbar__list')
+for (sec of sections) {
+
+var newEle = document.createElement("a")
+    
+navi.appendChild(newEle);
+
+
+// Scroll to section on link click
+// link between id and # to go to the section
+    newEle.innerHTML=`<li><a href = "#${sec.id}" data-nav="${sec.id}" class= "menu__link">${sec.id}</a></li>`
+    
+
+}
+
+
+/**
+ * Begin Events
+ *
+*/
+
+navi.addEventListener("click", (e)=>{
+    e.preventDefault();
+    if(e.target.dataset.nav){
+    document
+    .getElementById(`${e.target.dataset.nav}`)
+    .scrollIntoView({behavior: "smooth"});
+    }
+
+});
+
+
+
+
+// Set sections as active
+
+
+window.onscroll = scr => {
+
+    sections.forEach(function (active) {
+
+// Add class 'active' to section when near top of viewport
+
+        if (active.getBoundingClientRect().top >= -250 && active.getBoundingClientRect().top <= 250) {
+            active.classList.add("your-active-class");
+
+
+        }
+        else {
+            active.classList.remove("your-active-class");
+
+        }
+    }
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
